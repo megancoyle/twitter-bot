f21e9ff66d37f3c2077ed5d1bd81a0c945262a97
Megan Coyle <megan@mcoyle.com>
2018-07-21 15:03:46 -0400

    Successfully get bot to retweet specific tags

--- index.js
+++ index.js
@@ -1,22 +1,51 @@
 require("dotenv").config();
-var Twit = require("twit");
-var T = new Twit({
+
+const twit = require("twit");
+const config = {
   consumer_key: process.env.CONSUMER_KEY,
   consumer_secret: process.env.CONSUMER_SECRET,
   access_token: process.env.ACCESS_TOKEN,
   access_token_secret: process.env.ACCESS_TOKEN_SECRET
-});
-var users = ["MuseumModernArt", "hirshhorn", "ngadc"];
-var stream = T.stream("statuses/filter", { follow: users });
-stream.on("tweet", function(tweet) {
-  if (users.indexOf(tweet.user.id_str) > -1) {
-    console.log(tweet.user.name + ": " + tweet.text);
-    T.post("statuses/retweet/:id", { id: tweet.id_str }, function(
-      err,
-      data,
-      response
-    ) {
-      console.log(data);
-    });
-  }
-});
+};
+const Twitter = new twit(config);
+
+let retweet = function() {
+  let params = {
+    q: "#art, #painting",
+    result_type: "mixed",
+    lang: "en"
+  };
+  Twitter.get("search/tweets", params, function(err, data) {
+    // if there is no error
+    if (!err) {
+      // loop through the first 4 returned tweets
+      for (let i = 0; i < 4; i++) {
+        // iterate through those first four defining a rtId that is equal to the value of each of those tweets' ids
+        let rtId = data.statuses[i].id_str;
+        // the post action
+        Twitter.post(
+          "statuses/retweet/:id",
+          {
+            // setting the id equal to the rtId variable
+            id: rtId
+            // log response and log error
+          },
+          function(err, response) {
+            if (response) {
+              console.log("Successfully retweeted");
+            }
+            if (err) {
+              console.log(err);
+            }
+          }
+        );
+      }
+    } else {
+      // catch all log if the search could not be executed
+      console.log("Could not search tweets.");
+    }
+  });
+};
+
+retweet();
+setInterval(retweet, 600000);
